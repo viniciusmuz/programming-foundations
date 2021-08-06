d6e2bafb4513f9e26d2b061a2a79dd3324fab2a6
Vinicius Xavier <vinicius.muniz@ccc.ufcg.edu.br>
2021-08-05 22:25:06 -0300

    Using git on IntelliJ. Let's see.

---
+++ 1-foundations-w2/threeStripes.js
@@ -0,0 +1,18 @@
+const image = new SimpleImage("hilton.jpg");
+
+function threeStripes(image) {
+    for (var pixel of image.values()) {
+        if (pixel.getX() < (image.getWidth() / 3)) {
+            pixel.setRed(255);
+        }
+        else if (pixel.getX() < ((image.getWidth() / 3) * 2)) {
+            pixel.setGreen(255);
+        }
+        else {
+            pixel.setBlue(255);
+        }
+    }
+    return image;
+}
+
+print(threeStripes(image));
